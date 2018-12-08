9156a3b40a39b4d2ae3c23eb5d669d203145c59f
gensen <18027041863@163.com>
2018-12-08 08:10:08 +0800

    项目初始化

---
+++ src/main/java/com/cobee/mqttclientdemo/MqttclientdemoApp.java
@@ -0,0 +1,12 @@
+package com.cobee.mqttclientdemo;
+
+/**
+ * Created by Administrator on 2018/12/8.
+ */
+public class MqttclientdemoApp {
+
+    public static void main(String[] args) {
+        System.out.println("===============MqttclientdemoApp================");
+    }
+
+}
